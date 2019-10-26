34015572477dbd63750b60a2623905d30f6f8f67
Caitlan Jeffrey <caitlanjeffrey@gmail.com>
2019-10-26 11:17:20 -0700

    saving my changes

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -9,3 +9,40 @@ var firebaseConfig = {
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
+  var employeeName = "";
+  var employeeRole = "";
+  var startDate = "";
+  var monthlyRate = "";
+
+  $(".btn").on("click", function(event){
+    event.preventDefault();
+
+    employeeName = $("#inputEmployeeName").val().trim();
+    employeeRole = $("#inputRole").val().trim();
+    startDate = $("#inputStartDate").val().trim();
+    monthlyRate = $("#inputEmployeMonthlyRate").val().trim();
+
+    database.ref().push({
+      employeeName: employeeName,
+      employeeRole: employeeRole,
+      startDate: startDate,
+      monthlyRate: monthlyRate,
+    });
+  });
+
+    database.ref().on("value", function(snapshot){
+      console.log(snapshot.val());
+
+      console.log(snapshot.value().employeeName);
+      console.log(snapshot.value().employeeRole);
+      console.log(snapshot.value().startDate);
+      console.log(snapshot.value().monthlyRate);
+
+      $("#inputEmployeeName").text(snapshot.val().employeeName);
+
+    }, function(errorObject){
+      console.log("The read failed: " + errorObject);
+    });
