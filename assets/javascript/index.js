de223a2be6f9bb255dad3ac7e25dee1898f9c682
Loren Kolstad <lorenkolstad@gmail.com>
2019-10-26 11:02:08 -0700

    Styled the HTML a little more to match the assignment and linked Firebase script.

---
+++ assets/javascript/index.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAWPl0FbQKxSic1M35sIhCb9ba2dy_SONI",
+    authDomain: "employee-data-management-d4d14.firebaseapp.com",
+    databaseURL: "https://employee-data-management-d4d14.firebaseio.com",
+    projectId: "employee-data-management-d4d14",
+    storageBucket: "employee-data-management-d4d14.appspot.com",
+    messagingSenderId: "530804462349",
+    appId: "1:530804462349:web:00d2b91217b8abeb989644"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
